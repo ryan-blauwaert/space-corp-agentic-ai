f103094193ccbefbc54f1d2faf41a7da6b1361a1
ryanblauwaert <ryan.blauwaert@gmail.com>
2026-09-02 20:51:32 -0400

    feat: add health endpoint

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+
+from app.schemas.health import HealthResponse
 
 app = FastAPI()
+
+
+@app.get(
+    "/health",
+    response_model=HealthResponse,
+    status_code=status.HTTP_200_OK,
+)
+def health() -> HealthResponse:
+    return HealthResponse(status="ok")
